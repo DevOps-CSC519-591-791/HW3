5ac342b8cdd0b93dac2f914d9a125994045a0de5
Winbobob <zhu6@ncsu.edu>
2016-10-25 11:35:43 -0400

    Cat picture uploads: queue.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var redis = require('redis')
+var redis 	= require('redis')
 var multer  = require('multer')
 var express = require('express')
 var fs      = require('fs')
-var app = express()
+var app 	= express()
 // REDIS
 var client = redis.createClient(6379, '127.0.0.1', {})
 
@@ -16,42 +16,13 @@ app.use(function(req, res, next)
 	console.log(req.method, req.url);
 
 	// ... INSERT HERE.
+	// redis key should be string
 	client.lpush("recentUrl", req.url);
 	// Keep 5 recent urls
 	client.ltrim("recentUrl", 0, 4);
 	
 	next(); // Passing the request to the next handler in the stack.
 });
-
-app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
-   console.log(req.body) // form fields
-   console.log(req.files) // form files
-
-   if( req.files.image )
-   {
-	   fs.readFile( req.files.image.path, function (err, data) {
-	  		if (err) throw err;
-	  		var img = new Buffer(data).toString('base64');
-	  		console.log(img);
-		});
-	}
-	//store images to Redis
-   res.status(204).end()
-}]);
-
-app.get('/meow', function(req, res) {
-	{
-		if (err) throw err
-		res.writeHead(200, {'content-type':'text/html'});
-		items.forEach(function (imagedata) 
-		{
-   		res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
-		});
-
-	//pop up images
-   	res.end();
-	}
-})
 
 app.get('/', function(req, res) {
 	res.send('hello world!');
@@ -66,7 +37,8 @@ app.get('/set', function(req, res){
 app.get('/set/:key', function(req, res){
 	client.set("Zhewei", req.params.key);
 	client.expire("Zhewei", 10);
-	res.send("Key set."); //`Muat have res.send(), otherwise server will waiting response forever...`
+	//Must have res.send(), otherwise server will waiting response forever...
+	res.send("Key set."); 
 })
 
 app.get('/get', function(req, res){
@@ -80,12 +52,47 @@ app.get('/recent', function(req, res){
 	});
 })
 
+// Multer is a node.js middleware for handling multipart/form-data,
+// which is primarily used for uploading files.
+app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
+   console.log(req.body) // form fields
+   console.log(req.files) // form files
+
+   if( req.files.image )
+   {
+	   fs.readFile( req.files.image.path, function (err, data) {
+	  		if (err) throw err;
+	  		var img = new Buffer(data).toString('base64');
+	  		//store images to Redis
+	  		client.lpush("imageStack", img);
+	  		// Keep 3 recent images in stack
+	  		client.ltrim("imageStack", 0, 2);
+	  		console.log('Upload one image successfully!');
+		});
+	}
+   res.status(204).end()
+}]);
+
+app.get('/meow', function(req, res) {
+	
+	client.lrange("imageStack", 0, -1, function(err, items){
+		if (err) throw err
+		res.writeHead(200, {'content-type':'text/html'});
+		items.forEach(function (imagedata) 
+		{
+			res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+		});
+
+		res.end();
+	});
+})
+
 // HTTP SERVER
 var server = app.listen(3000, function () {
 
   var host = server.address().address
   var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Queues app listening at http://%s:%s', host, port)
 })
 
